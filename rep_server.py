fbb1fcf8fa04529aef67d3f46a0a25bef78cf0ec
ludel <ludovic.delsol@ynov.com>
2018-04-11 17:27:12 +0200

    sslerro

--- rep_server.py
+++ rep_server.py
@@ -8,4 +8,7 @@ def get_response(sites):
             dict_response[site[0]] = request('get', site[1]).status_code
         except exceptions.MissingSchema:
             dict_response[site[0]] = 404
+        except exceptions.SSLError:
+            dict_response[site[0]] = 400
+
     return dict_response
