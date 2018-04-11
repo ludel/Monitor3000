3801b5be05051daebd7ff73ee5958a384f99b719
ludel <ludovic.delsol@ynov.com>
2018-04-11 17:00:49 +0200

    404

--- rep_server.py
+++ rep_server.py
@@ -1,8 +1,11 @@
-from requests import request
+from requests import request, exceptions
 
 
 def get_response(sites):
     dict_response = {}
     for site in sites:
-        dict_response[site[0]] = request('get', site[1]).status_code
+        try:
+            dict_response[site[0]] = request('get', site[1]).status_code
+        except exceptions.MissingSchema:
+            dict_response[site[0]] = 404
     return dict_response
