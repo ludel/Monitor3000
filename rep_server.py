1805a5e95b15b5c2e7f86decf45cb6ae050891a0
ludel <ludovic.delsol@ynov.com>
2018-04-12 10:25:22 +0200

    error 400

--- rep_server.py
+++ rep_server.py
@@ -7,7 +7,7 @@ def get_response(sites):
         try:
             dict_response[site[0]] = request('get', site[1]).status_code
         except exceptions.MissingSchema:
-            dict_response[site[0]] = 404
+            dict_response[site[0]] = 400
         except exceptions.SSLError:
             dict_response[site[0]] = 495
         except exceptions.ConnectionError:
